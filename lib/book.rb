4c7ca3499f99b3f291c85a0f8b45f778944fe8d6
Alexander Riccio <test35965+learn@gmail.com>
2019-06-04 11:06:47 -0400

    solve part 1, book

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
-
-
+class Book
+    def initialize(book_name)
+        @title = book_name
+    end
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
